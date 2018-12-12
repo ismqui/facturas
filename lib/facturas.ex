5401c3ba4b011d94af155b6d0085dada1d72d3c4
ismqui <ismqui@gmail.com>
2018-12-12 20:21:25 +0100

    Initial commit

---
+++ lib/facturas.ex
@@ -0,0 +1,18 @@
+defmodule Facturas do
+  @moduledoc """
+  Documentation for Facturas.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Facturas.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/facturas_test.exs
@@ -0,0 +1,8 @@
+defmodule FacturasTest do
+  use ExUnit.Case
+  doctest Facturas
+
+  test "greets the world" do
+    assert Facturas.hello() == :world
+  end
+end
