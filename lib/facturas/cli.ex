8bde2640278aa58829665a07f075dd6a525b0964
ismqui <ismqui@gmail.com>
2019-06-10 20:52:03 +0200

    nada

--- lib/facturas/cli.ex
+++ lib/facturas/cli.ex
@@ -12,7 +12,7 @@ defmodule Facturas.CLI do
   @fichero  "facturas.csv"
   @dir  "/Users/ismqui/dev/elixir"
 
-  def main(_) do
+  def main() do
     IO.puts("Bienvenido al programa de facturas")
     print_help_message()
     FacturasFile.load("#{@dir}/#{@fichero}")
