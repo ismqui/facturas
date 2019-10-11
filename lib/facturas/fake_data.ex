56f2a2adca42ed000ecbe6ff8a0ad24c9909ca5e
ismqui <ismqui@gmail.com>
2019-10-12 00:45:34 +0200

    fake data improvement

--- lib/facturas/fake_data.ex
+++ lib/facturas/fake_data.ex
@@ -8,6 +8,9 @@ defmodule Facturas.FakeData do
     - 2018-12-21, 3, 2550.65, 21, 7, false, "Servicios Elixir"
   """
 
+  alias Facturas.Factura
+  alias Facturas.FacturasList
+
   def csv_file(val) when is_integer(val) and val > 0 do
     Enum.each(1..val, fn i ->
       fecha    = Faker.Date.backward(365)
@@ -27,14 +30,18 @@ defmodule Facturas.FakeData do
   end
   
   def list(val) when is_integer(val) and val > 0 do
-    Enum.into(1..val, [], fn i ->
-      fecha    = Faker.Date.backward(365)
-      precio   = (Faker.Commerce.price * 100) |> Float.round(2)
-      producto = Faker.Commerce.product_name
-      pagado   = Enum.random([true, false])
+    facturas =
+      Enum.into(1..val, [], fn i ->
+        fecha    = Faker.Date.backward(365)
+        precio   = (Faker.Commerce.price * 100) |> Float.round(2)
+        producto = Faker.Commerce.product_name
+        pagado   = Enum.random([true, false])
 
-      [fecha, i, precio, 21, 7, pagado, producto]
-    end)
+        Factura.crear(Date.to_iso8601(fecha), 1, precio, pagado, producto)
+      end)
+
+    FacturasList.new()
+    |> FacturasList.add_entries(facturas)
   end
 
   def list(_val) do
